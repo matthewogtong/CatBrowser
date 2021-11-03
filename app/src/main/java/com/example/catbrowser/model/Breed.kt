2b62e2c9e15b8e694ad2504129a56577db84b082
matthewogtong <ogtongm@gmail.com>
2021-11-03 15:46:39 -0400

    Make final modifications to breed model

--- app/src/main/java/com/example/catbrowser/model/Breed.kt
+++ app/src/main/java/com/example/catbrowser/model/Breed.kt
@@ -1,5 +1,9 @@
 package com.example.catbrowser.model
 
+import com.squareup.moshi.Json
+import com.squareup.moshi.JsonClass
+
+
 /**
  * {
 "weight": {
@@ -52,52 +56,53 @@ package com.example.catbrowser.model
  */
 
 // converting JSON into object
+@JsonClass(generateAdapter = true)
 data class Breed (
     val weight: Weight,
     val id: String,
     val name: String,
     val cfa_url: String,
-    val vetstreet_url: String,
-    val vcahospitals_url: String,
+    @Json(name = "vetstreet_url") val vetstreetUrl: String,
+    @Json(name = "vcahospitals_url") val vcahospitalsUrl: String,
     val temperament: String,
     val origin: String,
-    val country_codes: String,
-    val country_code: String,
+    @Json(name = "country_codes")val countryCodes: String,
+    @Json(name = "country_code")val countryCode: String,
     val description: String,
-    val life_span: String,
+    @Json(name = "life_span")val lifeSpan: String,
     val indoor: Int,
     val lap: Int,
-    val alt_names: String,
+    @Json(name = "alt_names")val altNames: String,
     val adaptability: Int,
-    val affection_level: Int,
-    val child_friendly: Int,
-    val dog_friendly: Int,
-    val energy_level: Int,
+    @Json(name = "affection_level")val affectionLevel: Int,
+    @Json(name = "child_friendly")val childFriendly: Int,
+    @Json(name = "dog_friendly")val dogFriendly: Int,
+    @Json(name = "energy_level")val energyLevel: Int,
     val grooming: Int,
-    val health_issues: Int,
+    @Json(name = "healthIssues") val healthIssues: Int,
     val intelligence: Int,
-    val shedding_level: Int,
+    @Json(name = "sheddingLevel") val sheddingLevel: Int,
     val social_needs: Int,
-    val stranger_friendly: Int,
+    @Json(name = "strangerFriendly") val strangerFriendly: Int,
     val vocalisation: Int,
     val experimental: Int,
     val hairless: Int,
     val natural: Int,
     val rare: Int,
     val rex: Int,
-    val suppressed_tail: Int,
-    val short_legs: Int,
-    val wikipedia_url: Int,
+    @Json(name = "suppressed_tail") val suppressedTail: Int,
+    @Json(name = "shortLegs") val shortLegs: Int,
+    @Json(name = "wikipedia_url")val wikipediaUrl: Int,
     val hypoallergenic: Int,
-    val reference_image_id: Int,
+    @Json(name = "reference_image_id")val referenceImageId: Int,
     val image: Image
 )
-
+@JsonClass(generateAdapter = true)
 data class Weight (
     val imperial: String,
     val metric: String,
 )
-
+@JsonClass(generateAdapter = true)
 data class Image (
     val id: String,
     val width: Int,
