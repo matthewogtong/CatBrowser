c00c63a0c3eb1b7c5ff1f2ae6c9703e741458ca0
matthewogtong <ogtongm@gmail.com>
2021-11-04 00:24:32 -0400

    Set up layouts

--- app/src/main/java/com/example/catbrowser/view/MainActivity.kt
+++ app/src/main/java/com/example/catbrowser/view/MainActivity.kt
@@ -18,7 +18,7 @@ class MainActivity : AppCompatActivity() {
 
         initViews()
 
-        viewModel.shibes.observe(this) {
+        viewModel.breeds.observe(this) {
             // Here is where your will get the result
             Log.d("MainActivity", "onCreate: $it")
             (binding.rvImages.adapter as ShibeAdapter).updateUrls(it)
