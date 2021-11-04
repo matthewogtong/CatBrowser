9c4c7d4d8d974eefcca1ea2e68e51194098171aa
matthewogtong <ogtongm@gmail.com>
2021-11-04 00:06:38 -0400

    Adjust CatRepo to return getBreeds method from catService

--- app/src/main/java/com/example/catbrowser/repo/CatRepo.kt
+++ app/src/main/java/com/example/catbrowser/repo/CatRepo.kt
@@ -1,11 +1,12 @@
 package com.example.catbrowser.repo
 
+import com.example.catbrowser.model.Breed
 import com.example.catbrowser.repo.remote.RetrofitInstance
 import retrofit2.Response
 
 object CatRepo {
 
-    suspend fun getImages(count: Int): Response<List<String>> {
-        return RetrofitInstance.catService.getImages(count, urls = true, httpsUrls = true)
+    suspend fun getBreeds(): Response<List<Breed>> {
+        return RetrofitInstance.catService.getBreeds()
     }
 }
