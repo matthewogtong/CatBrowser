4bbdc0154c33f79cccd862dc0726878e36687563
matthewogtong <ogtongm@gmail.com>
2021-11-04 14:10:32 -0400

    Create new detail activity for cat details

---
+++ app/src/main/java/com/example/catbrowser/view/DetailActivity.kt
@@ -0,0 +1,14 @@
+package com.example.catbrowser.view
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.catbrowser.databinding.ActivityDetailBinding
+
+class DetailActivity : AppCompatActivity() {
+
+    private val binding by lazy { ActivityDetailBinding.inflate(layoutInflater) }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+    }
+}
